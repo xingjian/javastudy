61601ab8e37bcb6a461a57ea0cf34d458795737b
xingjian <xingjian@yeah.net>
2016-06-21 17:17:24 +0800

    inint jersey framework

---
+++ SpringRestFrameWork/src/main/java/com/promisepb/rest/RestfulDemoService.java
@@ -0,0 +1,28 @@
+package com.promisepb.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.promisepb.service.DemoService;
+
+/**  
+ * 功能描述: 
+ * @author:<a href="mailto:xingjian@yeah.net">邢健</a>  
+ * @version: V1.0
+ * 日期:2016年6月21日 下午5:08:43  
+ */
+@Component
+@Path("/demo")
+public class RestfulDemoService {
+	@Autowired
+    private DemoService demoService;
+	
+	@GET
+    @Path("/say")
+    public String sayHello() {
+        return demoService.say();
+    }
+}
---
+++ SpringRestFrameWork/src/main/java/com/promisepb/service/DemoService.java
@@ -0,0 +1,11 @@
+package com.promisepb.service;
+
+/**  
+ * 功能描述: demo接口
+ * @author:<a href="mailto:xingjian@yeah.net">邢健</a>  
+ * @version: V1.0
+ * 日期:2016年6月21日 下午4:50:32  
+ */
+public interface DemoService {
+	String say();
+}
